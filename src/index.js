0181624447c2d08ec14654f33b377eb75e8551e0
Cesar Herrera <cesarherr321@gmail.com>
2023-05-15 20:51:23 -0400

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const main = document.querySelector('.container');
 const recentScore = document.createElement('section');
 recentScore.classList.add('scores');
 const subTitle = document.createElement('h2');
-subTitle.innerText = "Recent scores";
+subTitle.innerText = 'Recent scores';
 const refreshBtn = document.createElement('button');
-refreshBtn.innerText = "Refresh";
+refreshBtn.innerText = 'Refresh';
 const list = document.createElement('ul');
 
 main.appendChild(recentScore);
@@ -20,16 +20,16 @@ scoreList();
 const form = document.createElement('form');
 const nameInput = document.createElement('input');
 nameInput.classList.add('name');
-nameInput.type = "text";
-nameInput.placeholder = "Your name";
+nameInput.type = 'text';
+nameInput.placeholder = 'Your name';
 
 const scoreInput = document.createElement('input');
 scoreInput.classList.add('score');
-scoreInput.type = "text";
-scoreInput.placeholder = "Your Score";
+scoreInput.type = 'text';
+scoreInput.placeholder = 'Your Score';
 
 const scoreBtn = document.createElement('button');
-scoreBtn.innerText = "Submit"
+scoreBtn.innerText = 'Submit';
 scoreBtn.classList.add('submit-btn');
 
 main.appendChild(form);
